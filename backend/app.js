75f7cc78aecbf16f5ddc2548c61c1078a823ffe9
Deepak Kumar Ravi <dpakravi93@gmail.com>
2023-03-10 09:58:28 +0530

    Added cors

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:3000/',
-}))
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
